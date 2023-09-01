5f735659c2095397e3078611744f52cc41f426f4
Evan Chen <evan@evanchen.cc>
2023-08-31 21:25:16 -0400

    ci: lint.sh

---
+++ lint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -euo pipefail
+
+readarray -t SPELL_FILES_ARRAY < <(git ls-files)
+codespell "${SPELL_FILES_ARRAY[@]}"
+
+readarray -t PY_FILES_ARRAY < <(git ls-files '*.py')
+pyflakes "${PY_FILES_ARRAY[@]}"
+black --check "${PY_FILES_ARRAY[@]}"
+isort --check --profile black "${PY_FILES_ARRAY[@]}"
