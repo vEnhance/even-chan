fb34f2f79cbeca24416a7510fada0c3430a74786
Evan Chen <evan@evanchen.cc>
2023-08-31 20:53:48 -0400

    feat: start working on bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,75 @@
 import logging
-import os
+import re
+from typing import Any, Union
 
 import discord
-from discord import Message
+from discord import Embed, Message
 from discord.ext import commands
 from discord_slash import SlashCommand
 from discord_slash import SlashCommandOptionType as SCType
 from discord_slash import SlashContext
 from discord_slash.utils.manage_commands import create_option
 from dotenv import load_dotenv
+from von import api
 
 load_dotenv()
 
-GUILD_ID = int(os.getenv("GUILD_ID") or -1)
-assert GUILD_ID != -1
-GUILDS = [
-    GUILD_ID,
-]
+
+SOURCE_REGEX = re.compile(
+    r"(USAMO|JMO|IMO|Shortlist|ELMO|USA TST|TSTST|RMM) [0-9]{4}[ \/][ACGN]?[0-9]+"
+)
+
+# https://stackoverflow.com/questions/61150145/remove-line-breaks-but-not-double-line-breaks-python
+SINGLE_NEWLINE_DELETER = re.compile(r"(?<=[A-Za-z.:,\$ ]{2})\n(?!\n)")
+
+
+async def post_problem(source: str, trigger: Union[SlashContext, Message]):
+    if isinstance(trigger, SlashContext):
+        target = trigger
+    else:
+        target = trigger.channel
+
+    entry = api.get(source)
+    statement = entry.bodies[0]
+    statement = SINGLE_NEWLINE_DELETER.sub(" ", statement)
+    if len(statement) > 500:
+        statement = statement[:500]
+        if " " in statement[:500]:
+            i = statement.rindex(" ")
+            statement = statement[:i] + "..."
+        else:
+            statement += "..."
+
+    kwargs: dict[str, Any] = {
+        "title": source,
+        "description": "```latex" + "\n" + statement + "\n" + "```",
+    }
+    if entry.url is not None:
+        shortlink = entry.url
+        if shortlink.startswith("http://"):
+            shortlink = shortlink[7:]
+        if shortlink.startswith("https://"):
+            shortlink = shortlink[8:]
+        if shortlink.startswith("www."):
+            shortlink = shortlink[4:]
+        if len(shortlink) > 32:
+            shortlink = shortlink[:29] + "..."
+        kwargs["url"] = entry.url
+    else:
+        shortlink = None
+
+    embed = Embed(**kwargs)
+
+    if shortlink is not None:
+        embed.add_field(
+            name="Link",
+            value=f"[{shortlink}]({entry.url})",
+            inline=True,
+        )
+    if entry.author is not None:
+        embed.add_field(name="Author", value=entry.author, inline=True)
+
+    await target.send(embed=embed)  # type: ignore
 
 
 class EvenChanBot(commands.Bot):
@@ -30,7 +83,11 @@ class EvenChanBot(commands.Bot):
     async def on_message(self, message: Message):
         if message.author == self.user:
             return
-        # TODO
+        haystack = message.content.strip()
+        if (m := SOURCE_REGEX.search(haystack)) is not None:
+            source = m.group(0)
+            if api.has(source):
+                await post_problem(source, message)
 
 
 bot = EvenChanBot(command_prefix="&", intents=discord.Intents.default())
@@ -50,7 +107,12 @@ slash = SlashCommand(bot, sync_commands=True)
     ],
 )
 async def slash_yank(ctx: SlashContext, source: str):
-    pass  # TODO: source
+    if not SOURCE_REGEX.fullmatch(source):
+        await ctx.send(f"{source} is not in a supported contest", hidden=True)
+    elif not api.has(source):
+        await ctx.send(f"Could not find {source} in Evan's von database", hidden=True)
+    else:
+        await post_problem(source, ctx)
 
 
 @slash.slash(
